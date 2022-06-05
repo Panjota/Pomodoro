a041b12ec27d4414519bbfa059fb440823065032
Panjota <alexandro_pantoja@hotmail.com>
2022-06-05 18:27:41 -0400

    commit

--- main.py
+++ main.py
@@ -5,7 +5,6 @@ janela.title("Pomodoro")
 
 janela.geometry("300x300+550+200")
 janela.resizable(False,False)
-janela.iconbitmap("icon/icon_apple.ico")
 
 iniciar = Button(janela, text="Iniciar")
 iniciar.pack()
