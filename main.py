b42f5921fc6de5fbd597b16c91b5e7ff36676a42
Alexandro Pantoja <93011105+Panjota@users.noreply.github.com>
2022-06-05 15:56:12 -0400

    Update main.py

--- main.py
+++ main.py
@@ -1,3 +1,18 @@
 from tkinter import *
 
-print("pomodoro")
+janela = Tk()
+janela.title("Pomodoro")
+
+janela.geometry("300x300+550+200")
+janela.resizable(False,False)
+janela.iconbitmap("icon/icon_apple.ico")
+
+iniciar = Button(janela, text="Iniciar")
+iniciar.pack()
+pausar = Button(janela, text="Pausar")
+pausar.pack()
+resetar = Button(janela, text="Resetar")
+resetar.pack()
+
+
+janela.mainloop()
