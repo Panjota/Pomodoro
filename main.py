5e83f399282b5350f74203d3cc235217aa8256c6
Panjota <alexandro_pantoja@hotmail.com>
2022-06-06 17:21:58 -0400

    configurando inteface

--- main.py
+++ main.py
@@ -1,23 +1,34 @@
 from tkinter import *
-#from comandos import *
+import time
+
+foco = 25
+intervalo_curto = 5
+intervalo_longo = 15
+contagem_pomodoros = 0
+
+
+def iniciar():
+  pass
+def pausar():
+  pass
+def resetar():
+  pass
 
 janela = Tk()
 janela.title("Pomodoro")
-
-janela.geometry("300x300+550+200")
+janela.geometry("300x250+550+200")
 janela.resizable(False,False)
 
-#timermin =str(25)
-#timersec =str(20)
-
 tempo = Label(janela, text="00:00", font=("Times",72))
-tempo.pack()
-iniciar = Button(janela, width=6, text="Iniciar", font=("Comic Sans MS",15))
-iniciar.pack()
+tempo.place(x=35, y=30)
+contagem = Label(janela, text=f"Pomodoros:{contagem_pomodoros}", font=("Times",15))
+contagem.pack()
+iniciar = Button(janela, width=6, text="Iniciar", font=("Comic Sans MS",15), command=iniciar)
+iniciar.place(x=10, y=150)
 pausar = Button(janela, width=6, text="Pausar", font=("Comic Sans MS",15))
-pausar.pack()
+pausar.place(x=110, y=150)
 resetar = Button(janela, width=6, text="Resetar", font=("Comic Sans MS",15))
-resetar.pack()
+resetar.place(x=210, y=150)
 
 
 janela.mainloop()
