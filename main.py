eef2b085ea8523a886212054e87c8f68e5a44aa1
Alexandro Pantoja <93011105+Panjota@users.noreply.github.com>
2022-06-13 18:45:13 -0400

    add timer

--- main.py
+++ main.py
@@ -1,34 +1,79 @@
 from tkinter import *
+from tkinter import messagebox
 import time
 
-foco = 25
-intervalo_curto = 5
-intervalo_longo = 15
-contagem_pomodoros = 0
+cont = 0
 
+def intervalo_curto():
+  nome.config(text="Intervalo Curto")
+  minute_short = 1
+  sec_short = 60
+  while sec_short >= 0:
+    minuto.config(text=f"{minute_short}")
+    segundo.config(text=f"{sec_short}")
+    janela.update()
+    if minute_short < 10:
+      minuto.config(text=f"0{minute_short}")
+    if sec_short < 10: 
+      segundo.config(text=f"0{sec_short}")
+      janela.update()
+      time.sleep(0.1)
+      sec_short -= 1
+      print("sec_short")
 
 def iniciar():
-  pass
-def pausar():
-  pass
+  nome.config(text="Foco")
+  global cont
+  cont += 1 
+  contagem_pomodoros.set(f"Pomodoros: {cont}")
+  minute = 0
+  sec = 15
+  while sec >= 0:
+    minuto.config(text=f"{minute}")
+    segundo.config(text=f"{sec}")
+    if minute < 10:
+      minuto.config(text=f"0{minute}")
+    if sec < 10: 
+      segundo.config(text=f"0{sec}")
+    janela.update()
+    time.sleep(0.1)
+    sec -= 1
+    print(sec)
+    if sec == 0 and minute > 0:
+      sec = 60
+      minute -= 1
+    if minute == 0 and sec == 0:
+      messagebox.showinfo("Pomodoro", "Hora do intervalo curto")
+      #iniciar.config(command=intervalo_curto)
+      intervalo_curto()
+
+
 def resetar():
-  pass
+  janela.destroy()
 
 janela = Tk()
 janela.title("Pomodoro")
 janela.geometry("300x250+550+200")
 janela.resizable(False,False)
 
-tempo = Label(janela, text="00:00", font=("Times",72))
-tempo.place(x=35, y=30)
-contagem = Label(janela, text=f"Pomodoros:{contagem_pomodoros}", font=("Times",15))
-contagem.pack()
-iniciar = Button(janela, width=6, text="Iniciar", font=("Comic Sans MS",15), command=iniciar)
-iniciar.place(x=10, y=150)
-pausar = Button(janela, width=6, text="Pausar", font=("Comic Sans MS",15))
-pausar.place(x=110, y=150)
-resetar = Button(janela, width=6, text="Resetar", font=("Comic Sans MS",15))
-resetar.place(x=210, y=150)
+contagem_pomodoros = StringVar()
+contagem_pomodoros.set(f"Pomodoros: 0")
 
+nome = Label(janela, text="", font=("Times", 20))
+contagem = Label(janela, textvariable=contagem_pomodoros, font=("Times",15))
+minuto = Label(janela, text="25", font=("Times",72))
+separador = Label(janela,text=":", font=("Times",72))
+segundo = Label(janela, text="00", font=("Times", 72))
+iniciar = Button(janela, width=6, text="Iniciar", font=("Comic Sans MS",15), command=iniciar)
+resetar = Button(janela, width=6, text="Resetar", font=("Comic Sans MS",15), command=resetar)
+
+nome.pack()
+contagem.place(x=90, y=200)
+minuto.place(x=35, y=30)
+separador.place(x=130, y=30)
+segundo.place(x=160, y=30)
+iniciar.place(x=50, y=150)
+resetar.place(x=170, y=150)
 
 janela.mainloop()
+
